fd4ee70b00c9f6aa88f7054cbcd6e468a114084f
winnyDiese <winnydiese@gmail.com>
2024-12-26 13:53:49 +0100

    lkjn

--- src/app.ts
+++ src/app.ts
@@ -5,13 +5,19 @@ import router_user from "./routers/user.router"
 const app = express()
 const prisma = new PrismaClient()
 
+const corsOptions = {
+    origin: 'http://localhost:3000', 
+    optionsSuccessStatus: 200, // Réglage pour d'anciens navigateurs
+    credentials: true 
+};
+
+
 app.use(express.json())
-
-
+app.use(core(corsOptions))
 app.use('/api', router_user)
 
 
 // Demarer le serveur
-app.listen(3000, ()=>{
-    console.log('Server is running on http://localhost:3000')
+app.listen(3001, ()=>{
+    console.log('Server is running on http://localhost:3001')
 })
